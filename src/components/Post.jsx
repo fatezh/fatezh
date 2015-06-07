9816525aa625cc083d30111519c755977810dde3
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-07 21:46:44 +0300

    run post contents through markdown

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,18 @@
 import React from "react"
 import PostHeader from "PostHeader"
+import Markdown from "marked"
 
 export default class Post extends React.Component {
+  getPostContents() {
+    return { __html: Markdown(this.props.contents) }
+  }
+
   render() {
     return(
       <article className="post">
         <PostHeader title={this.props.title} date={this.props.date} />
 
-        <section className="post-contents" dangerouslySetInnerHTML={{ __html: this.props.contents }}>
+        <section className="post-contents" dangerouslySetInnerHTML={this.getPostContents()}>
         </section>
       </article>
     )
