9965b0fc972633fd3c2de0ccec89406630693bec
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-07 19:31:01 +0300

    sketch page header

--- src/application.jsx
+++ src/application.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import ApplicationContainer from "ApplicationContainer"
+import ApplicationHeader from "ApplicationHeader"
 import ConstructionSign from "ConstructionSign"
 
 React.render(
   <ApplicationContainer>
+    <ApplicationHeader />
     <ConstructionSign />
   </ApplicationContainer>,
   document.getElementById("application-root")
