120b9c3c42361b87003800cf6292a37ea21b209e
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-07 19:32:00 +0300

    drop ConstructionSign component

--- src/application.jsx
+++ src/application.jsx
@@ -1,12 +1,10 @@
 import React from "react"
 import ApplicationContainer from "ApplicationContainer"
 import ApplicationHeader from "ApplicationHeader"
-import ConstructionSign from "ConstructionSign"
 
 React.render(
   <ApplicationContainer>
     <ApplicationHeader />
-    <ConstructionSign />
   </ApplicationContainer>,
   document.getElementById("application-root")
 )
