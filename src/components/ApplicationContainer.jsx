59564eb12a2c9345e8768b5f09ad07d34671bda6
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-09 00:12:35 +0300

    sketch permalinks

--- src/components/ApplicationContainer.jsx
+++ src/components/ApplicationContainer.jsx
@@ -1,10 +1,14 @@
 import React from "react"
+import Router from "react-router"
+const { RouteHandler } = Router
+import ApplicationHeader from "ApplicationHeader"
 
 export default class ApplicationContainer extends React.Component {
   render() {
     return (
       <main className="application_container">
-        {this.props.children}
+        <ApplicationHeader />
+        <RouteHandler/>
       </main>
     )
   }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,7 +15,7 @@ export default class Post extends React.Component {
   render() {
     return(
       <article className="post">
-        <PostHeader title={this.props.title} date={this.props.date} />
+        <PostHeader title={this.props.title} date={this.props.date} slug={this.props.slug} />
 
         <section className="post-contents" dangerouslySetInnerHTML={this.getPostContents()}>
         </section>
