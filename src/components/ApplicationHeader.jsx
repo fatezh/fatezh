5fda8b4349fbe0b6b14fd03314ddceb803683477
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-09 00:15:52 +0300

    add 'Home' link to page header

--- src/components/ApplicationHeader.jsx
+++ src/components/ApplicationHeader.jsx
@@ -1,11 +1,14 @@
 import React from "react"
+import Router from "react-router"
+const { Link } = Router
+
 
 export default class ApplicationHeader extends React.Component {
   render() {
     return(
       <header className="application_header">
         <h1 className="application_header-title">
-          Верстающий Фатеж
+          <Link to="/">Верстающий Фатеж</Link>
         </h1>
       </header>
     )
