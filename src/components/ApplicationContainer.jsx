9f033d88a67855fe30ba1fc28e1b45a2f2ba83db
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-10 20:40:46 +0300

    animate page transitions

--- src/components/ApplicationContainer.jsx
+++ src/components/ApplicationContainer.jsx
@@ -1,15 +1,26 @@
-import React from "react"
+import React from "react/addons"
 import Router from "react-router"
 const { RouteHandler } = Router
+const { CSSTransitionGroup } = React.addons
 import ApplicationHeader from "ApplicationHeader"
 
-export default class ApplicationContainer extends React.Component {
+class ApplicationContainer extends React.Component {
   render() {
+    const name = this.context.router.getCurrentPath()
+
     return (
       <main className="application_container">
         <ApplicationHeader />
-        <RouteHandler />
+        <CSSTransitionGroup component="div" transitionName="application_container-body" transitionLeave={false}>
+          <RouteHandler key={name} />
+        </CSSTransitionGroup>
       </main>
     )
   }
 }
+
+ApplicationContainer.contextTypes = {
+  router: React.PropTypes.func.isRequired
+}
+
+export default ApplicationContainer
