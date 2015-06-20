901be1b0e07241b4de83193e4e96ec7a1f1cbee3
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-20 18:58:42 +0300

    rearrange posts

--- src/posts/index.js
+++ src/posts/index.js
@@ -2,22 +2,22 @@ var fs = require("fs")
 
 export default [
   {
-    title: "Рубрика. Советы от Павла Глоблы",
-    slug: "3-horoscope",
-    contents: fs.readFileSync(__dirname + "/3-horoscope.html", "utf-8"),
-    date: "26 июня 2015",
+    title: "Заклинаю вас, откажитесь от CSS",
+    slug: "3-deprecate-your-css",
+    contents: fs.readFileSync(__dirname + "/3-deprecate-your-css.html", "utf-8"),
+    date: "20 июня 2015",
     author: {
-      name: "Павел Глобла",
-      img: "/images/globa.jpg",
-      link: "https://vk.com/pavsekakii",
-      bio: "Маг, экстрасенс, носитель положительной энергетики и трансцендентная личность"
+      name: "Джен Шиффер",
+      img: "/images/jenn.jpeg",
+      link: "https://medium.com/cool-code-pal/a-call-for-web-developers-to-deprecate-their-css-1f6430781393",
+      bio: "Веб-разработчик, теоретик сложных вычислений. Автор “PHP: The Hood Parts” и других программно-фантастических бестселлеров на Amazon Kindle и Zune"
     }
   },
   {
     title: "Топ 5 ошибок в CSS",
     slug: "3-top-5-css-mistakes",
     contents: fs.readFileSync(__dirname + "/3-top-5-css-mistakes.html", "utf-8"),
-    date: "26 июня 2015",
+    date: "20 июня 2015",
     author: {
       name: "Джен Шиффер",
       img: "/images/jenn.jpeg",
@@ -26,15 +26,15 @@ export default [
     }
   },
   {
-    title: "Заклинаю вас, откажитесь от CSS",
-    slug: "3-deprecate-your-css",
-    contents: fs.readFileSync(__dirname + "/3-deprecate-your-css.html", "utf-8"),
-    date: "26 июня 2015",
+    title: "Рубрика. Советы от Павла Глоблы",
+    slug: "3-horoscope",
+    contents: fs.readFileSync(__dirname + "/3-horoscope.html", "utf-8"),
+    date: "20 июня 2015",
     author: {
-      name: "Джен Шиффер",
-      img: "/images/jenn.jpeg",
-      link: "https://medium.com/cool-code-pal/a-call-for-web-developers-to-deprecate-their-css-1f6430781393",
-      bio: "Веб-разработчик, теоретик сложных вычислений. Автор “PHP: The Hood Parts” и других программно-фантастических бестселлеров на Amazon Kindle и Zune"
+      name: "Павел Глобла",
+      img: "/images/globa.jpg",
+      link: "https://vk.com/pavsekakii",
+      bio: "Маг, экстрасенс, носитель положительной энергетики и трансцендентная личность"
     }
   },
   {
