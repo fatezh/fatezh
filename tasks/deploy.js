08bdc0ecf65b892c7007931fde1163e18550ea72
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-25 22:03:35 +0300

    convert deploy task to es6

---
+++ tasks/deploy.js
@@ -0,0 +1,17 @@
+import AppConfig from "../config/application"
+
+import gulp from "gulp"
+import deploy from "gulp-gh-pages"
+
+const REMOTE_URL = "git@github.com:fatezh/fatezh.github.io.git"
+const REMOTE_BRANCH = "master"
+
+gulp.task("deploy", ["build"], () => {
+  gulp.src(AppConfig.buildpaths.deploy)
+    .pipe(deploy({ remoteUrl: REMOTE_URL, branch: REMOTE_BRANCH }))
+})
+
+gulp.task("deploy:ci", ["build"], () => {
+  gulp.src(AppConfig.buildpaths.deploy)
+    .pipe(deploy({ remoteUrl: process.env.GH_REPO_URL, branch: REMOTE_BRANCH }))
+})
