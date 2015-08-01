70c516cd5bd5024ae82f29900eba895f9a73a409
Vasily Polovnyov <vasily@polovnyov.ru>
2015-08-01 14:33:43 +0300

    use HistoryLocation only in production

--- src/application.js
+++ src/application.js
@@ -2,6 +2,14 @@ import React from "react"
 import Router from "react-router"
 import routes from "./components/ApplicationRouter"
 
-Router.run(routes, Router.HistoryLocation, (Handler) => {
+const routerHistory = () => {
+  if (document.body.className.indexOf("in-production") >= 0) {
+    return Router.HistoryLocation
+  } else {
+    return null
+  }
+}
+
+Router.run(routes, routerHistory(), (Handler) => {
   React.render(<Handler />, document.getElementById("application-root"))
 })
