2e0acac688f6307a08d9d54b974ed140a26ee801
Vasily Polovnyov <vasily@polovnyov.ru>
2015-08-01 11:45:11 +0300

    extract ApplicationRouter

--- src/application.jsx
+++ src/application.jsx
@@ -1,17 +1,7 @@
 import React from "react"
 import Router from "react-router"
-const { Route, DefaultRoute } = Router
-import ApplicationContainer from "ApplicationContainer"
-import PostsList from "PostsList"
-import SinglePost from "SinglePost"
+import routes from "ApplicationRouter"
 
-const routes = (
-  <Route name="app" path="/" handler={ApplicationContainer}>
-    <Route name="single-post" path="/posts/:slug" handler={SinglePost} />
-    <DefaultRoute handler={PostsList} />
-  </Route>
-)
-
-Router.run(routes, function(Handler) {
+Router.run(routes, (Handler) => {
   React.render(<Handler />, document.getElementById("application-root"))
 })
---
+++ src/components/ApplicationRouter.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Router from "react-router"
+const { Route, DefaultRoute } = Router
+
+import ApplicationContainer from "ApplicationContainer"
+import PostsList from "PostsList"
+import SinglePost from "SinglePost"
+
+export default (
+  <Route name="app" path="/" handler={ApplicationContainer}>
+    <Route name="single-post" path="/posts/:slug" handler={SinglePost} />
+    <DefaultRoute handler={PostsList} />
+  </Route>
+)
