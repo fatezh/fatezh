eec7914c29d0c018d266eadeeec1424d7e81aa10
Vasily Polovnyov <vasily@polovnyov.ru>
2015-08-01 11:08:08 +0300

    now we need react/addongs only for css transitions

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import React from "react/addons"
+import React from "react"
 import PostHeader from "PostHeader"
 import PostFooter from "PostFooter"
 
--- src/components/PostFooter.jsx
+++ src/components/PostFooter.jsx
@@ -1,4 +1,4 @@
-import React from "react/addons"
+import React from "react"
 import PostAuthor from "PostAuthor"
 
 export default class extends React.Component {
