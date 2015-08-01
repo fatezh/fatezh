36f202cc3b0ba848fdd5344b7259364eb2db3696
Vasily Polovnyov <vasily@polovnyov.ru>
2015-08-01 14:41:15 +0300

    fix deploy tasks: return the stream

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -7,11 +7,11 @@ const REMOTE_URL = "git@github.com:fatezh/fatezh.github.io.git"
 const REMOTE_BRANCH = "master"
 
 gulp.task("deploy", () => {
-  gulp.src(AppConfig.buildpaths.deploy)
+  return gulp.src(AppConfig.buildpaths.deploy)
     .pipe(deploy({ remoteUrl: REMOTE_URL, branch: REMOTE_BRANCH }))
 })
 
 gulp.task("deploy:ci", () => {
-  gulp.src(AppConfig.buildpaths.deploy)
+  return gulp.src(AppConfig.buildpaths.deploy)
     .pipe(deploy({ remoteUrl: process.env.GH_REPO_URL, branch: REMOTE_BRANCH }))
 })
