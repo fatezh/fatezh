fdf0a82a2323466a75a9e83afa7b1da832b93add
Vasily Polovnyov <vasily@polovnyov.ru>
2015-08-01 14:04:59 +0300

    fix deploy tasks deps: they should be run manually

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -6,12 +6,12 @@ import deploy from "gulp-gh-pages"
 const REMOTE_URL = "git@github.com:fatezh/fatezh.github.io.git"
 const REMOTE_BRANCH = "master"
 
-gulp.task("deploy", ["build"], () => {
+gulp.task("deploy", () => {
   gulp.src(AppConfig.buildpaths.deploy)
     .pipe(deploy({ remoteUrl: REMOTE_URL, branch: REMOTE_BRANCH }))
 })
 
-gulp.task("deploy:ci", ["build"], () => {
+gulp.task("deploy:ci", () => {
   gulp.src(AppConfig.buildpaths.deploy)
     .pipe(deploy({ remoteUrl: process.env.GH_REPO_URL, branch: REMOTE_BRANCH }))
 })
